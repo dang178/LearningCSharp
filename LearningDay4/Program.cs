d5c5a25e5ae82618e979d6b6e6c5712d67dcd8d5
dang178 <kuangdang178@163.com>
2017-11-28 18:21:03 +0800

    day4+day5

---
+++ LearningDay4/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningDay4
+{
+    class Program
+    {
+        class TestA
+        {
+            public int a;
+            public string b;
+            public static TestA operator + (TestA lhs, TestA rhs)   //重载运算符
+            {
+                TestA result = new TestA();
+                result.a = lhs.a + rhs.a;
+                result.b = lhs.b + rhs.b;
+                return result;
+            }
+        }
+        struct TestB
+        {
+            public int a;
+            public string b;
+        }
+        static void Main (string[] args)
+        {
+            int? b = null;  //定义可空类型
+            int a = b ?? 0; //合并空运算符
+            Console.WriteLine("a:{0},b:{1}", a, b);
+
+            //类的比较
+            TestA testA = new TestA();
+            testA.a = 1;
+            testA.b = "s";
+            TestA testB = new TestA();
+            testB.a = 1;
+            testB.b = "s";
+            if (ReferenceEquals(testA, testB))  //判断两个类是否引用同一个对象，即是否指向同一个地址
+            {
+                Console.WriteLine("两个对象相同！");
+            }
+            else
+            {
+                Console.WriteLine("两个对象不同！");
+            }
+
+            if (Equals(testA, testB))
+            {
+                Console.WriteLine("equals,两个对象相同！");
+            }
+            else
+            {
+                Console.WriteLine("equals,两个对象不同！");
+            }
+            Console.WriteLine("testA+testB={0}", (testA + testB).a);
+            //结构的比较
+            TestB testC = new TestB();
+            testC.a = 1;
+            testC.b = "s";
+            TestB testD = new TestB();
+            testD.a = 1;
+            testD.b = "s";
+            if (ReferenceEquals(testC, testD))  //判断两个类是否引用同一个对象，即是否指向同一个地址
+            {
+                Console.WriteLine("两个对象相同！");
+            }
+            else
+            {
+                Console.WriteLine("两个对象不同！");
+            }
+
+            if (Equals(testC, testD))
+            {
+                Console.WriteLine("equals,两个对象相同！");
+            }
+            else
+            {
+                Console.WriteLine("equals,两个对象不同！");
+            }
+            //unsafe
+            //{
+            //    Console.WriteLine(sizeof(TestA));//sizeof适用于不安全模式
+            //}
+            //byte a = 255;
+            //checked     //执行溢出检查，当溢出时，会抛出异常
+            //{
+            //    a++;
+            //}
+            //unchecked { //禁止溢出检查
+            //    a++;
+            //}
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LearningDay5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningDay5
+{
+    class Program
+    {
+        public static int add (int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int sub (int a, int b)
+        {
+            return a - b;
+        }
+        public delegate int delegate1 (int a, int b);//委托定义
+        public event delegate1 event1;
+        static void Main (string[] args)
+        {
+            delegate1 dl1 = new delegate1(add);
+            dl1 += new delegate1(sub);//多播委托。
+            Console.WriteLine("1+3={0}", dl1(1, 3));
+            Console.WriteLine("3-1={0}", dl1(3, 1));    //由于委托的顺序不确定,造成输出错误
+            foreach (var dl in dl1.GetInvocationList()) //循环多播委托的方法
+            {
+                Console.WriteLine("1,3={0}", (dl as delegate1)(3, 1));
+            }
+            Func<int, int, int> dl2 = delegate (int a, int b)   //匿名方法。单行定义委托，有返回值
+            {
+                return a + b;
+            };
+            Console.WriteLine("2+4={0}", dl2(2, 4));
+            Action<string> dl3 = str => //无返回值委托，lambda表达式
+            {
+                Console.WriteLine(str += "!");
+            };
+            dl3("hello world");
+            
+            
+            Console.ReadKey();
+        }
+    }
+}
